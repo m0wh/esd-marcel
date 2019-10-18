5ee072e0abe124eb1974bae90709364d8e1418b9
Malo Widerspach <malo.dewiderspach@gmail.com>
2019-10-18 16:29:06 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,9 @@ set -e
 yarn build
 cd dist
 
-# if deploying to a custom domain
-# echo 'domain.com' > CNAME
-
 git init
 git add -A
 git commit -m 'Update' --allow-empty
-git push -f origin master
-# you can replace 'origin' by the repo you want to commit to (e.g. git push -f git@github.com:m0wh/m0wh.github.io.git master:master)
+git push -f git@github.com:m0wh/esd-marcel.git master:gh-pages
 
 cd -
